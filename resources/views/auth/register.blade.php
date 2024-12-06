fedee5e49427ce6d90ad8ffc0990e3cf26cd4a30
Thuhuong2210 <phanthuhuong520@gmail.com>
2024-12-06 19:16:10 +0700

    update dang ky

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,6 +5,16 @@
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
+            <div class="form-group mb-3">
+                    <label class="form-label">Nhập thông tin của bạn:</label>
+                    <div>
+                        <input type="radio" name="loai_tk" id="gia_su" value="Gia sư" checked>
+                        <label for="gia_su">Gia sư</label>
+                        <input type="radio" name="loai_tk" id="hoc_vien" value="Học viên">
+                        <label for="hoc_vien">Học viên / Phụ huynh </label>
+                    </div>
+            </div>
+
             <div class="form-group">
                 <label for="TenDN">Tên Đăng Nhập</label>
                 <input type="text" name="TenDN" id="TenDN" class="form-control" required>
