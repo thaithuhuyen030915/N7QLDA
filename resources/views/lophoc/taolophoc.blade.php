ba1ae0bef59576782140700dcece15adec8e21ed
ThanNgocLan2003 <24a4043062@hvnh.edu.vn>
2024-12-15 11:52:34 +0700

    upcode

--- resources/views/lophoc/taolophoc.blade.php
+++ resources/views/lophoc/taolophoc.blade.php
@@ -1,7 +1,5 @@
 <!DOCTYPE html>
 <html lang="vi">
-
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,8 +10,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
    
 </head>
-
-
 <body>
 @if ($errors->any())
         <div class="alert alert-danger">
